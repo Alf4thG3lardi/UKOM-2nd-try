8f39e5712eb64b6b34e7a793d86bc06a54ecbceb
fey <111243780+feisa11@users.noreply.github.com>
2023-10-18 14:08:56 +0700

    Add files via upload

---
+++ src/Home page/pricing/pricing.js
@@ -0,0 +1,257 @@
+import React from 'react';
+
+
+const Pricing = () => {
+  return (
+    <div class='container'>
+      <h1 class='text-white text-center mb-4 display-4 fw-semibold' style={{paddingTop:"90px"}}>Pricing</h1>
+      <section style={{paddingTop:"50px"}}>
+      <h3 class='' style={{color: "#3457AF"}}>WEB INSTANT</h3>
+      <p class='text-light text-left'>Harga Web Instan adalah salah satu fitur yang sangat penting dalam situs web penyedia jasa pembuatan website. Dalam dunia digital yang terus berkembang, para pengguna yang ingin mendirikan situs web mereka membutuhkan kemudahan dan transparansi dalam menentukan biaya yang diperlukan untuk menghasilkan proyek web mereka. Di bawah ini adalah deskripsi untuk bagian Harga Web Instan pada situs web ini:</p>
+      <div class="row" style={{paddingTop:"45px"}}>
+        <div class="col">
+          <div class="card h-100">
+            <h5 class='card-header text-light text-center' style={{background: "#3457AF"}}>EKONOMIS</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 30.000/bulan</h5>
+              <ul>
+                <li>
+                  Setup 3 Halaman Situs
+                </li>
+                <li>
+                  Revisi Desain 1x
+                </li>
+                <li>
+                  Edit Halaman 1x/3 Bulan
+                </li>
+                <li>
+                  Gratis domain *.com
+                </li>
+                <li>
+                  Gratis SSL Selamanya
+                </li>
+                <li>
+                  1 Email Bisnis Profesional
+                </li>
+                <li>
+                  Cloudflare Edge Network
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#3457AF"}}>BASIC</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 60.000/bulan</h5>
+              <ul>
+                <li>
+                  Setup 6 Halaman Situs
+                </li>
+                <li>
+                  Revisi Desain 1x
+                </li>
+                <li>
+                  Edit Halaman 1x/2 Bulan
+                </li>
+                <li>
+                  Gratis domain *.com
+                </li>
+                <li>
+                  Gratis SSL Selamanya
+                </li>
+                <li>
+                  3 Email Bisnis Profesional
+                </li>
+                <li>
+                  Cloudflare Edge Network
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#3457AF"}}>PREMIUM</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 90.000/bulan</h5>
+              <ul>
+                <li>
+                  15 Halaman Awal
+                </li>
+                <li>
+                  Revisi Desain 1x
+                </li>
+                <li>
+                  Edit Halaman 1x/1 Bulan
+                </li>
+                <li>
+                  Gratis Domain *.com
+                </li>
+                <li>
+                  Gratis SSL Selamanya
+                </li>
+                <li>
+                  10 Email Bisnis Profesional
+                </li>
+                <li>
+                  Cloudflare Edge Network
+                </li>
+                <li>
+                  Gratis 10 Artikel 500 Kata
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#3457AF"}}>BUSINESS</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 170.000/bulan</h5>
+              <ul>
+                <li>
+                 25 Halaman Awal
+                </li>
+                <li>
+                  Revisi Desain 1x
+                </li>
+                <li>
+                  Edit Halaman 1x/1 Bulan
+                </li>
+                <li>
+                  Gratis domain *.com
+                </li>
+                <li>
+                  Gratis SSL Selamanya
+                </li>
+                <li>
+                  50 Email Bisnis Profesional
+                </li>
+                <li>
+                  Cloudflare Edge Network
+                </li>
+                <li>
+                  Gratis 25 Artikel 500 Kata
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+      <div class="container" style={{paddingTop:"80px"}}>
+      <h3 class='' style={{color: "#3457AF"}}>WEB CUSTOM</h3>
+      <p class='text-light text-left'>Harga Web Custom adalah fitur utama dalam situs web kami yang ditujukan bagi pengguna yang ingin membuat situs web sesuai dengan kebutuhan dan visi khusus mereka. Kami memahami bahwa setiap proyek web unik, dan itulah mengapa kami menawarkan solusi kustom yang dapat disesuaikan dengan segala kebutuhan. Berikut adalah deskripsi bagian Harga Web Custom*:</p>
+      <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#3457AF"}}>Pembuatan Website Custom</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 40.000/bulan</h5>
+              <ul>
+                <li>
+                  Mendapatkan Homepage
+                </li>
+                <li>
+                  Gratis Gambar (Icon, Logo,)
+                </li>
+                <li>
+                  Membuat Bagian Sesuai Permintaan Pelanggan (Halaman yang Dibutuhkan)
+                </li>
+                <li>
+                  Gratis Tema/Sesuai Minat Pelanggan
+                </li>
+              </ul>
+            </div>
+          </div>
+      </div>
+      <section style={{paddingTop:"50px"}}>
+      <h3 class='text-white text-center'>Tambahan Layanan</h3>
+      <div class="row" style={{paddingTop:"45px"}}>
+        <div class="col">
+          <div class="card h-100">
+            <h5 class='card-header text-light text-center' style={{background: "#7893D8"}}>Tambah Halaman</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 100.000/halaman</h5>
+              <p class='text-center'>Kami dapat membantu Anda membuat tambahan konten artikel dalam website</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#7893D8"}}>Publish & Format Artikel</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 70.000/halaman revisi</h5>
+              <p class='text-center'>Kami bisa bantu anda mem-format dan mem-publish artikel yang sudah anda siapkan ke situs.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#7893D8"}}>Revisi konten</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 50.000/halaman revisi</h5>
+              <p class='text-center'>Update data konten dalam website Anda untuk informasi terbaru yang mungkin berubah</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+      <div class="row" style={{paddingTop:"45px"}}>
+      <section style={{paddingTop:"10px"}}>
+      <div class="row" style={{paddingTop:"45px"}}>
+        <div class="col">
+          <div class="card h-100">
+            <h5 class='card-header text-light text-center' style={{background: "#7893D8"}}>Integrasi Google Search Console</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 250.000</h5>
+              <p class='text-center'>Membantu Anda memantau, mempertahankan, dan memecahkan masalah kehadiran situs Anda di hasil Google Penelusuran</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#7893D8"}}>Integrasi Google Analytics</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 100.000</h5>
+              <p class='text-center'>Menampilkan statistik pengunjung sebuah situs web untuk informasi update data statistik web</p>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card h-100">
+          <h5 class='card-header text-light text-center' style={{background: "#7893D8"}}>Integrasi Google My Business</h5>
+            <div class="card-body">
+              <h5 class="card-title text-center">Rp 500.000</h5>
+              <p class='text-center'>Google My Business mendukung usaha multi lokasi bila kamu punya cabang di tempat lain dengan nama dan usaha yang sama</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+      </div>
+      <p class='text-danger' style={{paddingTop:"50px"}}>*Syarat dan Ketentuan Berlaku</p>
+      <section class='text-light'>
+      <ul>
+        <li>
+          Waktu Pengerjaan 3-7 Hari
+        </li>
+        <li>
+          Mengirimkan Link Web Rujukan (Website Contoh)
+        </li>
+        <li>
+          Pengerjaan Setelah Data/Materi Dikirimkan (Logo, Profil, DLL)
+        </li>
+        <li>
+          Revisi Sampai Hasil Maksimal (Tidak Merubah Tema yang Dipilih)
+        </li>
+        <li>
+          Pembayaran Dimuka untuk Tanda Kerja Sama
+        </li>
+      </ul>
+      </section>
+    </div>
+  )
+}
+
+export default Pricing;
