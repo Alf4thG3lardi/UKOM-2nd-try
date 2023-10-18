5c6d12f7feeaa3c32151aaa2d3d3bb79227c714e
UlulAzmi111 <azmiulul1453@gmail.com>
2023-10-18 11:33:58 +0700

    Merevisi gambar pilihan web custom

--- src/Features/Web Custom/konten/konten.js
+++ src/Features/Web Custom/konten/konten.js
@@ -107,7 +107,7 @@ const Konten = () => {
         </div>
         <div class="carousel-inner">
           <div class="carousel-item active" data-bs-interval="10000">
-            <img src={sekolah} class="d-block w-100 mx-auto" />
+            <img src={lowongan} class="d-block w-100 mx-auto" />
             <div class="d-none d-md-block" style={{ marginTop: "70px" }}>
               <h5 className="text-white" style={{ fontSize: "30px" }}>
                 Web Portal Lowongan Pekerjaan
@@ -118,7 +118,7 @@ const Konten = () => {
             </div>
           </div>
           <div class="carousel-item" data-bs-interval="2000">
-            <img src={lowongan} class="d-block w-100 mx-auto" />
+            <img src={sekolah} class="d-block w-100 mx-auto" />
             <div class="d-none d-md-block" style={{ marginTop: "70px" }}>
               <h5 className="text-white" style={{ fontSize: "30px" }}>
                 Web Sekolah, Yayasan, dan Universitas
